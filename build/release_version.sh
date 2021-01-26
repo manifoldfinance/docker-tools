56d401aa62d5b9f66a80728b324d75f27b5d1c56
sam bacha <sam@freighttrust.com>
2021-01-25 18:50:03 -0800

    build(utils): versioning

---
+++ build/release_version.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# release_versioning.sh
+# semver docker images 
+versions=()
+
+for versionGroup in "$@"; do
+
+	# Process the version group(s) that were passed to this script.
+	if [[ "$versionGroup" == *"#"* ]]; then
+		vgParam1=$(cut -d "#" -f2- <<< "$versionGroup")
+		versionGroup="${versionGroup//$vgParam1}"
+		versionGroup="${versionGroup//\#}"
+	fi
+
+	if [[ "$versionGroup" == *"="* ]]; then
+		vgAlias1=$(cut -d "=" -f2- <<< "$versionGroup")
+		versionGroup="${versionGroup//$vgAlias1}"
+		versionGroup="${versionGroup//=}"
+	fi
+
+	vgVersion=$(cut -d "v" -f2- <<< "$versionGroup")
+	versions+=( "$vgVersion" )
+done
+
+branchName=""
+commitMSG=""
+
+if [[ ${#versions[@]} == 0 ]]; then
+	echo "Error, no versions detected."
+	exit 1
+elif [[ ${#versions[@]} == 1 ]]; then
+	branchName="release-v${versions[0]}"
+	commitMSG="Publish v${versions[0]}. [release]"
+elif [[ ${#versions[@]} == 2 ]]; then
+	branchName="release-v${versions[0]}-and-v${versions[1]}"
+	commitMSG="Publish v${versions[0]} and v${versions[1]}. [release]"
+elif [[ ${#versions[@]} == 3 ]]; then
+	branchName="release-v${versions[0]}-v${versions[1]}-and-more"
+	commitMSG="Publish v${versions[0]}, v${versions[1]}, and v${versions[2]}. [release]"
+elif [[ ${#versions[@]} == 4 ]]; then
+	branchName="release-v${versions[0]}-v${versions[1]}-and-more"
+	commitMSG="Pub: v${versions[0]}, v${versions[1]}, v${versions[2]}, and more. [release]"
+elif [[ ${#versions[@]} -gt 4 ]]; then
+	branchName="release-v${versions[0]}-v${versions[1]}-and-more"
+	commitMSG="Pub: ${versions[0]},${versions[1]},${versions[2]},${versions[3]}, and more. [release]"
+fi
+
+git checkout -b "${branchName}" master
+shared/gen-dockerfiles.sh "$@"
+git add .
+git commit -m "${commitMSG}"
+git push -u origin "${branchName}"
